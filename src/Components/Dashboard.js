294ba0f4a040abbede5bfba1e5549ca25688b573
tlinq <lindqvist.tobias.erik@gmail.com>
2019-12-14 10:28:02 +0000

    removed the cookicutter stuff from create react app

---
+++ src/Components/Dashboard.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class Dashboard extends Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {  };
+    // }
+    render() {
+        return (
+            <div>Data goes here</div>
+        );
+    }
+}
+
+export default Dashboard;
